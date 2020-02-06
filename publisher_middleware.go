630ff6db68ff6fcce88e5fb2298aab7c0d42ba52
gcode <thienthanh11tclc@gmail.com>
2020-02-06 14:14:23 +0700

    [PRD-639] add middleware to publisher

---
+++ publisher_middleware.go
@@ -0,0 +1,25 @@
+package redisqueue
+
+import (
+	"context"
+	"fmt"
+
+	newrelic "github.com/newrelic/go-agent"
+)
+
+type PublisherHandlerFunc func(ctx context.Context, job Job) error
+
+type PublisherHandlerMiddleWare func(next PublisherHandlerFunc) PublisherHandlerFunc
+
+func WithNewRelicTransaction() PublisherHandlerMiddleWare {
+	return func(next PublisherHandlerFunc) PublisherHandlerFunc {
+		return func(ctx context.Context, job Job) error {
+			txn := newrelic.FromContext(ctx)
+			segment := newrelic.StartSegment(txn, fmt.Sprintf("%s-%s", job.GetQueue(), job.GetID()))
+			defer func() {
+				_ = segment.End()
+			}()
+			return next(ctx, job)
+		}
+	}
+}
